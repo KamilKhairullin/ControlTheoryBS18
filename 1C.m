8fb1bf0e530763e7bcf28ac8c4be82e2fa14179b
KamilKhairullin <kamil.khayrullin.personal@yandex.ru>
2021-11-05 17:08:23 +0300

    control-theory

--- 1C.m
+++
@@ -1,8 +0,0 @@
-clc;
-clear all;
-
-syms x(t) t
-equation = dsolve('D2x(t)+2*Dx(t)-3*x(t)=sin(4*t)', 'x(0) = 2', 'Dx(0) = 3', 't')
-pretty(equation)
-ezplot(equation,[0,10])
-
--- 4_matlab.m
+++
@@ -1,4 +0,0 @@
-numerator = [1, 4];
-denominator = [1,3,15];
-sys = tf(numerator,denominator)
-rltool(sys)
